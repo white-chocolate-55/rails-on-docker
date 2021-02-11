63a685cc6b471917c7258a7049c1355213b979a8
white-chocolate-55 <kazki.kurita@gmail.com>
2021-02-11 21:05:11 +0900

    sample app

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,10 @@ bundle check || true
 
 yarn check --integrity --silent || true
 
+if [ "${1:-}" = rails -a "${2}:-" = server ]; then
+  if [ -f tmp/pids/server.pid ]; then
+    rm -v tmp/pids/server.pid
+  fi
+fi
+
 exec "$@"
